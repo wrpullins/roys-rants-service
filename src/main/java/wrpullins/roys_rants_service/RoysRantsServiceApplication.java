f95c4ba45ab712279b849fe542686a78ab44606f
Roy Pullins <w.r.pullins@gmail.com>
2026-01-14 20:21:34 +1300

    Initial commit - spring init and nothing else

---
+++ src/main/java/wrpullins/roys_rants_service/RoysRantsServiceApplication.java
@@ -0,0 +1,13 @@
+package wrpullins.roys_rants_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoysRantsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoysRantsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wrpullins/roys_rants_service/RoysRantsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wrpullins.roys_rants_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoysRantsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
